71a0c1adb4c7d026f1cb2a3d546577feb4ec5564
pranav280601 <72141278+pranav280601@users.noreply.github.com>
2021-04-14 10:01:46 +0530

    Add files via upload

---
+++ Command_Line_Argument.py
@@ -0,0 +1,17 @@
+import sys
+
+
+def Addition(x,y):
+	return x+y
+
+
+def main():
+	if len(sys.argv)<2 or len(sys.argv)>2:
+		print("Invalid number of arguments ")
+		return
+
+	ret = Addition(int(sys.argv[1]),int(sys.argv[2]))	
+	print("Addition is:",ret)
+
+if __name__ == "__main__" :
+	main()	
